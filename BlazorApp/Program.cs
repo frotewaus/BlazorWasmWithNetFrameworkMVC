c978453136ff6248c76115874524e6dfc6325765
michalw <michalw@rite.us>
2022-02-18 18:17:17 +0100

    Changes that allow using Blazor components directly

--- BlazorApp/Program.cs
+++ BlazorApp/Program.cs
@@ -1,12 +1,10 @@
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
 using System;
-using System.Collections.Generic;
 using System.Net.Http;
-using System.Text;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components.Web;
+using BlazorApp.Components;
 
 namespace BlazorApp
 {
@@ -15,8 +13,13 @@ namespace BlazorApp
         public static async Task Main(string[] args)
         {
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
-            builder.RootComponents.Add<App>("#app");
-
+#if STANDALONE
+            builder.RootComponents.Add<AppDebug>("#app");
+#else
+            builder.RootComponents.RegisterForJavaScript<Counter>(identifier: "counter", javaScriptInitializer: "loadCounter");
+            builder.RootComponents.RegisterForJavaScript<Components.Index>(identifier: "index", javaScriptInitializer: "loadIndex");
+            builder.RootComponents.RegisterForJavaScript<FetchData>(identifier: "fetchData", javaScriptInitializer: "loadFetchData");
+#endif
             builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
             await builder.Build().RunAsync();
---
+++ WebApplication5/Controllers/WeatherController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication5.Controllers
+{
+    public class WeatherForecast
+    {
+        public string Date { get; set; }
+
+        public int TemperatureC { get; set; }
+
+        public string Summary { get; set; }
+
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+    }
+
+    public class WeatherController : Controller
+    {
+        public ActionResult Index()
+        {
+            var items = new List<WeatherForecast>();
+            items.Add(new WeatherForecast { Date = "2021-01-02", TemperatureC = 33 });
+            items.Add(new WeatherForecast { Date = "2022-05-12", TemperatureC = 25 });
+
+            return Json(items, JsonRequestBehavior.AllowGet);
+        }
+    }
+}
